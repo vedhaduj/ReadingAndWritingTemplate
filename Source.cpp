70263793e8c3f444ef6da04e949821b5fc9f874c
vedhaduj <97368023+vedhaduj@users.noreply.github.com>
2022-07-01 20:02:14 -0400

    Create Source.cpp

---
+++ Source.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <Windows.h>
+
+void ReadM(HANDLE process, LPCVOID address, LPVOID output)
+{
+	ReadProcessMemory(process, (LPCVOID)address, output, sizeof(output), nullptr);
+}
+void WriteM(HANDLE process, LPVOID destination, LPVOID source)
+{
+	WriteProcessMemory(process, destination, source, sizeof(source), 0);
+}
+
+HANDLE GetProcess()
+{
+	DWORD pid;
+	HANDLE process{};
+	HWND window = FindWindowA(0, "AssaultCube");
+	//lazy moment
+	if (!window)
+	{
+		std::cout << "Window Error\n";
+	}
+	GetWindowThreadProcessId(window, &pid);
+	process = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
+	if (!process)
+	{
+		std::cout << "Process Error\n";
+	}
+	return process;
+}
+
+
+int main()
+{
+	int outty{};
+	int newammo = 5000;
+	HANDLE Process = GetProcess();
+	ReadM(Process, LPCVOID(0x00DDAB50), &outty);
+	std::cout << outty;
+	WriteM(Process, LPVOID(0x00DDAB50), &newammo);
+	ReadM(Process, LPCVOID(0x00DDAB50), &outty);
+	std::cout << outty;
+}
